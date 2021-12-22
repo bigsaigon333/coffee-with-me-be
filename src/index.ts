66f5848b10444319ddc7c5bae71e77e1d2e2a12e
bigsaigon333 <likepeppermint@gmail.com>
2021-12-22 11:59:07 +0900

    feat: express server가 / route의 요청에 응답한다

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import express from "express";
+import { PORT } from "./constants";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  console.log("/");
+  res.send(`Hello world! ${Date.now()}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Express Server running at http://localhost:${PORT}`);
+});
